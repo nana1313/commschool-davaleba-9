99478411a6846224443e62bd4c75def44809c308
nana dumbadze <55438500+nana1313@users.noreply.github.com>
2024-01-23 20:53:28 +0400

    add first and second excercise

---
+++ app.js
@@ -0,0 +1,13 @@
+// davaleba 1
+// let a = Number(prompt("შეიყვანე პირველი რიცხვი."));
+// let b = Number(prompt("შეიყვანე მეორე რიცხვი."));
+// alert(`ჯამი = ${a + b}; ნამრავლი = ${a * b}`);
+
+// davaleba 2
+alert(5 > 4);                  // true
+alert("apple" > "pineapple");  //false
+alert("2" > "12");             //true
+alert(undefined == null);      //true
+alert(undefined === null);     //false
+alert(null == " 0 ");          //false
+alert(null === + "0");         //fase
